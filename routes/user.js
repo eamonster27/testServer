7d3f3189e8cc6749fa9e010d2d16fa9a171bdb03
EamonBachari <eamonbachari@gmail.com>
2017-10-10 13:23:21 -0500

    changes to server.js

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express'),
       models  = require('../models');
 
+const router = express.Router();
 
 router.get('/users', function(req, res, next){
   models.User.findAll().then(users => {
     res.json(users);
   });
 })
+
+module.exports = router;
